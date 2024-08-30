5f41c40c84efa9376c8ed2d4a833832b4227f02a
yhj9585 <85930226+yhj9585@users.noreply.github.com>
2024-08-31 01:11:44 +0900

    [Silver II] Title: N과 M (11), Time: 168 ms, Memory: 1272 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#pragma warning (disable : 4996)
+
+#define SIZE 10005
+
+int s[SIZE];
+int tp;
+int arr[SIZE];
+
+// stack //
+void push(int n) {
+	s[tp++] = n;
+}
+
+void pop() {
+	tp = tp - 1;
+}
+
+int size() {
+	return tp;
+}
+
+void printStack() {
+	for (int i = 0; i < tp; i++) {
+		printf("%d ", arr[s[i]]);
+	}
+	printf("\n");
+}
+
+
+int N, M;
+int visit[SIZE];
+int before[SIZE];
+
+void num(int t, int a) {
+	//(1)
+	//if (visit[a] == 1 || arr[a] == before[t]) return;
+	if (arr[a] == before[t]) return;
+	push(a);
+	visit[a] = 1;
+
+	if (size() != M) {
+		for (int n = 1; n <= N; n++) {
+		//for (int n = a+1; n <= N; n++) {
+		//for (int n = a; n <= N; n++) {		
+			num(t + 1, n);
+		}
+	}
+
+	if (size() == M) {
+		printStack();
+	}
+
+	for (int i = 1; i <= N; i++) {
+		before[i] = 0;
+	}
+
+
+	//visit[a] = 0;
+	pop();
+	before[t] = arr[a];
+}
+
+int temp;
+
+int main() {
+	scanf("%d %d", &N, &M);
+
+	for (int i = 1; i <= N; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (arr[i] == arr[i - 1]) {
+			continue;
+		}
+		num(1, i);
+	
+	}
+
+}
