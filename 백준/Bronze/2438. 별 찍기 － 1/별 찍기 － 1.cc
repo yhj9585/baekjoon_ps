55a19d1096e8c935727e174fec7b3d25a2c45689
yhj9585 <85930226+yhj9585@users.noreply.github.com>
2024-08-31 00:53:32 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#pragma warning (disable: 4996)
+
+
+
+int main() {
+	int N = 0;
+
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	return 0;
+
+
+
+}
