3fa1501f82965c4fb8198737c835861c33b8c033
yhj9585 <85930226+yhj9585@users.noreply.github.com>
2024-08-31 01:13:07 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 896 ms, Memory: 485584 KB -BaekjoonHub

---
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.cc
@@ -0,0 +1,153 @@
+#include <stdio.h>
+
+int N; int M; int breakable;
+int map[1003][1003] = {0,};
+//int chk[2][1003][1003] = {0,};
+int brokenchk[3][1003][1003] = {0,};
+const int NOTGO = 20;
+
+int move[4][2] = {
+    {-1, 0},
+    { 1, 0},
+    { 0,-1},
+    { 0, 1} 
+};
+
+typedef struct pos {
+    int y;
+    int x;
+    int broken;
+    int depth;
+}pos;
+
+int st = 0; int ed = 0;
+pos queue[30000000] = {0,};
+void push(pos node) {
+    queue[st++] = node;
+    return;
+}
+pos pop() {
+    return queue[ed++];
+}
+int size() {
+    return st-ed;
+}
+
+
+int check(pos nxtpos, int day, int nxtday) {
+    if (nxtpos.y >= N || nxtpos.y < 0 || nxtpos.x >= M || nxtpos.x < 0) {
+        return 0;
+    }
+    else if (map[nxtpos.y][nxtpos.x] == 0 && brokenchk[nxtday][nxtpos.y][nxtpos.x] > nxtpos.broken) {
+        return 1;
+    }
+    else if (map[nxtpos.y][nxtpos.x] == 1 && day == 1 && brokenchk[nxtday][nxtpos.y][nxtpos.x] > nxtpos.broken+1) {
+        if (nxtpos.broken == breakable) {
+            return 0;
+        }
+        return 2;
+    }
+    
+    return 0;
+}
+/*
+void pri() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            printf("%d/%d ", chk[0][i][j], chk[1][i][j]);
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            printf("%2d/%2d ", brokenchk[0][i][j], brokenchk[1][i][j]);
+        }
+        printf("\n");
+    }
+    return;
+}
+*/
+
+void bfs() {
+    int day;
+    int nxtday;
+    pos nowpos;
+    pos nxtpos;
+    while (size() != 0) {
+        nowpos = pop();
+        day = nowpos.depth%2; // 1 is day 0 is night
+        //printf("[%d,%d] Depth : %d\n", nowpos.y, nowpos.x, nowpos.depth);
+        //pri();
+
+        
+        if (nowpos.y == N-1 && nowpos.x == M-1) {
+            //pri();
+            printf("%d\n", nowpos.depth);
+            return;
+        }
+        
+        nxtday = (day+1)%2;
+        for (int i = 0; i < 4; i++) {
+            nxtpos.y = nowpos.y + move[i][0];
+            nxtpos.x = nowpos.x + move[i][1];
+            nxtpos.depth = nowpos.depth + 1;
+            nxtpos.broken = nowpos.broken;
+            if (check(nxtpos, day, nxtday) == 1) {
+                //chk[(day+1)%2][nxtpos.y][nxtpos.x] = nxtpos.depth;
+                brokenchk[nxtday][nxtpos.y][nxtpos.x] = nxtpos.broken;
+                push(nxtpos);
+
+                if (nxtpos.y == N-1 && nxtpos.x == M-1) {
+                    //pri();
+                    printf("%d\n", nxtpos.depth);
+                    return;
+                }
+            }
+            else if (check(nxtpos, day, nxtday) == 2) {
+                //chk[(day+1)%2][nxtpos.y][nxtpos.x] = nxtpos.depth;
+                nxtpos.broken = nxtpos.broken+1;
+                brokenchk[nxtday][nxtpos.y][nxtpos.x] = nxtpos.broken;
+                push(nxtpos);
+            }
+        }
+        if (/*chk[(day+1)%2][nowpos.y][nowpos.x] == 0 && */brokenchk[nxtday][nowpos.y][nowpos.x] > nowpos.broken) {
+            nxtpos.y = nowpos.y;
+            nxtpos.x = nowpos.x;
+            nxtpos.depth = nowpos.depth + 1;
+            nxtpos.broken = nowpos.broken;
+            //chk[(day+1)%2][nxtpos.y][nxtpos.x] = nxtpos.depth;
+            brokenchk[nxtday][nowpos.y][nowpos.x] = nxtpos.broken;
+            push(nxtpos);
+        }
+    }
+
+    printf("-1\n");
+
+    return;
+}
+
+
+int main() {
+    scanf("%d %d %d", &N, &M, &breakable);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            scanf("%1d", &map[i][j]);
+            brokenchk[0][i][j] = NOTGO;
+            brokenchk[1][i][j] = NOTGO;
+        }
+    }
+
+    pos newpos;
+    newpos.y = 0;
+    newpos.x = 0;
+    newpos.broken = 0;
+    newpos.depth = 1;
+    //chk[1][0][0] = 1;
+    brokenchk[1][0][0] = 0;
+    push(newpos);
+
+    bfs();
+
+    return 0;
+
+}
