ae039d70606fac3db71a32fafa219ae399661985
yhj9585 <85930226+yhj9585@users.noreply.github.com>
2024-08-31 01:10:29 +0900

    [Silver III] Title: N과 M (3), Time: 476 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#pragma warning (disable : 4996)
+
+#define SIZE 100
+
+int s[SIZE];
+
+int tp;
+
+// stack //
+void push(int n) {
+	s[tp++] = n;
+}
+
+void pop() {
+	tp--;
+}
+
+int top() {
+	return s[tp];
+}
+
+int size() {
+	return tp;
+}
+
+void printStack() {
+	for (int i = 0; i < tp; i++) {
+		printf("%d ", s[i]);
+	}
+	printf("\n");
+}
+
+
+void cleanStack() {
+	while (size() != 0) {
+		pop();
+	}
+}
+
+int N, M;
+int visit[SIZE];
+int round;
+
+void num(int t, int a) {
+	//if (visit[a] == 1) return;
+	push(a);
+	//visit[a] = 1;
+
+
+	if (size() != M) {
+		
+		for (int n = 1; n <= N; n++) {
+		//for (int n = a+1; n <= N; n++) {
+			num(t + 1, n);
+		}
+	}
+
+	if (size() == M) {
+		printStack();
+	}
+	//visit[a] = 0;
+	pop();
+}
+
+
+
+int main() {
+	scanf("%d %d", &N, &M);
+	
+	for (int i = 1; i <= N; i++) {
+		num(1, i);
+	}
+
+}
